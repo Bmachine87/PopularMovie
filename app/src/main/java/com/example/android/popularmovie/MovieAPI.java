093f16dbbf4763646856dd7e5c31eebb3106cb4c
ben <blee@pandora.com>
2018-11-10 02:37:36 -0800

    Update MovieAdapter logic

--- app/src/main/java/com/example/android/popularmovie/MovieAPI.java
+++ app/src/main/java/com/example/android/popularmovie/MovieAPI.java
@@ -1,4 +1,11 @@
 package com.example.android.popularmovie;
 
 public class MovieAPI {
+
+    public static final String API_URL = "http://api.themoviedb.org/3/movie";
+    // TODO: Compele the botom three
+    public static final String IMAGE_URL = "";
+    public static final String IMAGE_SIZE = "";
+    public static final String IMAGE_NOT_FOUND = "";
+
 }
--- app/src/main/java/com/example/android/popularmovie/MovieAdapter.java
+++ app/src/main/java/com/example/android/popularmovie/MovieAdapter.java
@@ -1,4 +1,64 @@
 package com.example.android.popularmovie;
 
-public class MovieAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MovieAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public MovieAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    public static class ViewHolder {
+        ImageView mImageViewMovie;
+    }
+    @Override
+    public int getCount() {
+        return Movie.movieImages.size().;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(final int i, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        //TODO: Review this logic
+        if(convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext()
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+
+            viewHolder = new ViewHolder();
+
+            viewHolder.mImageViewMovie = (ImageView) convertView.findViewById(R.id.iv_image);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        //TODO: Import picasso and review for placeholder, error, into
+        Picasso.with(mContext).load(Movie.movieImages.get(position))
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error)
+                .into(viewHolder.mImageViewMovie);
+
+        return convertView;
+    }
 }
