7490fc9da1446ae2e52e5108c1e87fda005bb129
ben <blee@pandora.com>
2018-11-12 23:53:36 -0800

    Update movie class with member variables and get methods

--- app/src/main/java/com/example/android/popularmovie/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovie/MainActivity.java
@@ -3,7 +3,12 @@ package com.example.android.popularmovie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static ArrayList<Movie> movieList;
+    public static ArrayList<String> movieImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/android/popularmovie/Movie.java
+++ app/src/main/java/com/example/android/popularmovie/Movie.java
@@ -1,4 +1,42 @@
 package com.example.android.popularmovie;
 
 public class Movie {
+    // Set up member variables for the movie
+    String mTitle;
+    String mPopularity;
+    String mRating;
+    String mDescription;
+    String mReleaseDate;
+
+    public Movie(String movieTitle, String moviePopularity, String movieRating,
+                 String movieDescription, String movieReleaseDate) {
+        mTitle = movieTitle;
+        mPopularity = moviePopularity;
+        mRating = movieRating;
+        mDescription = movieDescription;
+        mReleaseDate = movieReleaseDate;
+    }
+
+    // Set up get methods
+
+    public String getmTitle() {
+        return mTitle;
+    }
+
+    public String getmPopularity() {
+        return mPopularity;
+    }
+
+    public String getmRating() {
+        return mRating;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public String getmReleaseDate() {
+        return mReleaseDate;
+    }
+
 }
