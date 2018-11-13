bc77bca955457ba1fedf53fda935ec05ee5b9bf9
ben <blee@pandora.com>
2018-11-13 00:30:14 -0800

    Implement Parcelable: used as constructor for Movie, and writeToParcel override method, and override describeContents method. Implement method for Parcelable.Creator to create object and array with parcel. Update with a method to build a poster path and added setter methods

--- app/src/main/java/com/example/android/popularmovie/Movie.java
+++ app/src/main/java/com/example/android/popularmovie/Movie.java
@@ -1,42 +1,99 @@
 package com.example.android.popularmovie;
 
-public class Movie {
-    // Set up member variables for the movie
-    String mTitle;
-    String mPopularity;
-    String mRating;
-    String mDescription;
-    String mReleaseDate;
+import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public Movie(String movieTitle, String moviePopularity, String movieRating,
+public class Movie implements Parcelable {
+    // Set up member variables for the movie
+    private int mID;
+    private String mTitle;
+    private String mPoster;
+    private String mRating;
+    private String mDescription;
+    private String mReleaseDate;
+    private static final String MOVIE_POSTER_URL = "http://image.tmdb.org/t/p/";
+
+    /**
+     * BASE CONSTRUCTOR
+     * @param id integer id of the movie
+     * @param movieTitle string of the movie title
+     * @param moviePoster string of the path of movie poster
+     * @param movieRating string of the movie rating average
+     * @param movieDescription string of the movie synopsis
+     * @param movieReleaseDate string of the movie release date
+     */
+    public Movie(int id, String movieTitle, String moviePoster, String movieRating,
                  String movieDescription, String movieReleaseDate) {
+        mID = id;
         mTitle = movieTitle;
-        mPopularity = moviePopularity;
+        mPoster = moviePoster;
         mRating = movieRating;
         mDescription = movieDescription;
         mReleaseDate = movieReleaseDate;
     }
 
-    // Set up get methods
-
-    public String getmTitle() {
-        return mTitle;
+    /**
+     * Constructor used by the Saved Instance
+     * @param parcel the object that contains the movie data
+     */
+    private Movie(Parcel parcel) {
+        mID = parcel.readInt();
+        mTitle = parcel.readString();
+        mPoster = parcel.readString();
+        mRating = parcel.readString();
+        mDescription = parcel.readString();
+        mReleaseDate = parcel.readString();
     }
 
-    public String getmPopularity() {
-        return mPopularity;
+    // Set up get/set methods for class properties
+    public int getmID() { return mID; }
+    public void setmID(int id) { mID = id; }
+
+    public String getmTitle() { return mTitle; }
+    public void setmTitle(String movieTitle) { mTitle = movieTitle; }
+
+    public String getmPoster() { return mPoster; }
+    public void setmPoster(String moviePoster) { mPoster = moviePoster; }
+
+    public String getmRating() { return mRating; }
+    public void setmRating(String movieRatings) { mRating = movieRatings;}
+
+    public String getmDescription() { return mDescription; }
+    public void setmDescription(String movieDescription) { mDescription = movieDescription; }
+
+    public String getmReleaseDate() { return mReleaseDate; }
+    public void setmReleaseDate(String movieReleaseDate) { mReleaseDate = movieReleaseDate; }
+
+    /**
+     * This method returns the complete poster path based on screen size
+     *
+     * @param context application context
+     * @return the path used by the Picasso library to display an image
+     */
+    public String buildPosterPath(Context context) {
+        String posterWidth = context.getResources().getString(R.string.poster_size);
+        return MOVIE_POSTER_URL + posterWidth + getmPoster();
     }
 
-    public String getmRating() {
-        return mRating;
+    @Override
+    public int describeContents() { return 0; }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeInt(mID);
+        parcel.writeString(mTitle);
+        parcel.writeString(mPoster);
+        parcel.writeString(mRating);
+        parcel.writeString(mDescription);
+        parcel.writeString(mReleaseDate);
     }
 
-    public String getmDescription() {
-        return mDescription;
-    }
+    public static final Parcelable.Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel parcel) { return new Movie(parcel); }
 
-    public String getmReleaseDate() {
-        return mReleaseDate;
-    }
-
+        @Override
+        public Movie[] newArray(int i) { return new Movie[i]; }
+    };
 }
