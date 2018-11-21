8089b5a2074b4b5023196514e9071effcf332537
ben <blee@pandora.com>
2018-11-21 00:13:54 -0800

    Update MainActivity - incomplete. Uploading to continue work on another machine

--- app/src/main/java/com/example/android/popularmovie/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovie/MainActivity.java
@@ -1,8 +1,20 @@
 package com.example.android.popularmovie;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.GridView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -10,12 +22,54 @@ public class MainActivity extends AppCompatActivity {
 
     public static ArrayList<Movie> movieList;
     public static ArrayList<String> movieImages;
-
+    public static MovieAdapter movieAdapter;
+    public static String sortBy;
+    public static GridView movieGridView;
     public static Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        android.support.v7.widget.Toolbar toolbar =
+                (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.framelayout, new MovieFragment())
+                    .commit();
+        }
     }
+
+    // Creates OptionMenu in mainactivity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.recent) {
+            startActivity(new Intent(this, SortMovieActivity.class));
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class MovieFragment extends Fragment {
+        public MovieFragment() {}
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/android/popularmovie/SortMovieActivity.java
@@ -0,0 +1,4 @@
+package com.example.android.popularmovie;
+
+class SortMovieActivity {
+}
