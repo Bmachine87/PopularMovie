78b17900ea85b6038270f67e88f8e1e0bcdce480
bmachine <benjamin.lt.lee@gmail.com>
2018-11-25 12:25:09 -0800

    Update MainActivity code, add osort by strings to strings.xml

--- app/src/main/java/com/example/android/popularmovie/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovie/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.android.popularmovie;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,10 +13,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.Toast;
-import android.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     public static ArrayList<String> movieImages;
     public static MovieAdapter movieAdapter;
     public static String sortBy;
+    public static String lastSortBy;
     public static GridView movieGridView;
     public static Toast toast;
 
@@ -38,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.framelayout, new MovieFragment())
+                    .add(R.id.fragment_movie_details, new MovieFragment())
                     .commit();
         }
     }
@@ -67,9 +69,68 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-            return super.onCreateView(inflater, container, savedInstanceState);
+            View rootV = inflater.inflate(R.layout.movie_fragment, container, false);
+            setHasOptionsMenu(true);
+
+            movieGridView = (GridView) rootV.findViewById(R.id.gridview_movie);
+            //TODO: Verify you do not need to setNumColumns or set orientation
+            movieGridView.setAdapter(movieAdapter);
+            movieGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                    Intent intent = new Intent(getActivity(), MovieDetailsActivity.class);
+                    intent.putExtra("movie_id", movieList.get(position).getmID());
+                    intent.putExtra("movie_position", position);
+                    startActivity(intent);
+                }
+            });
+
+            toast = Toast.makeText(rootV.getContext(),"",Toast.LENGTH_SHORT);
+            return rootV;
+        }
+
+        @Override
+        public void onSaveInstanceState(@NonNull Bundle outState) {
+            outState.putParcelableArrayList("movies", MainActivity.movieList);
+            outState.putStringArrayList("images", MainActivity.movieImages);
+            super.onSaveInstanceState(outState);
+        }
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            if(savedInstanceState != null && savedInstanceState.containsKey("movies")) {
+                movieList = savedInstanceState.getParcelableArrayList("movies");
+                movieImages = savedInstanceState.getStringArrayList("images");
+            } else {
+                movieList = new ArrayList<Movie>();
+                movieImages = new ArrayList<String>();
+                movieAdapter = new MovieAdapter(getActivity());
+                updateMovieList();
+            }
+            super.onCreate(savedInstanceState);
+        }
+
+        //TODO: Update with SharedPreferences
+        private void updateMovieList() {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            sortBy = sharedPrefs.getString(getString(R.string.sort_by), getString(R.string.sort_by_default));
+            new FetchMovieTask().execute(sortBy, null);
+
+        }
+
+        //TODO: Update MovieFragment onResume (requires SharedPreferences
+        @Override
+        public void onResume() {
+            SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            sortBy = sharedPrefs.getString(getString(R.string.sort_by), getString(R.string.sort_by_default));
+
+            if(lastSortBy != null && !sortBy.equals(lastSortBy)) {
+                movieList = new ArrayList<Movie>();
+                movieImages = new ArrayList<String>();
+                updateMovieList();
+            }
+            lastSortBy = sortBy;
+            super.onResume();
         }
     }
-
 
 }
