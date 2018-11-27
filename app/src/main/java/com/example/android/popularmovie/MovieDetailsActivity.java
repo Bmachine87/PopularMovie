52a521fbca0ee124b700765fb10a0b66f3e40a95
bmachine <benjamin.lt.lee@gmail.com>
2018-11-26 22:26:04 -0800

    update manifest to include settings and moviedetails

--- app/src/main/java/com/example/android/popularmovie/MovieDetailsActivity.java
+++ app/src/main/java/com/example/android/popularmovie/MovieDetailsActivity.java
@@ -1,4 +1,93 @@
 package com.example.android.popularmovie;
 
-public class MovieDetailsActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+    public static Movie movie;
+    public static Intent intent;
+    public static TextView title, release, rating, synopsis;
+    public static ImageView poster;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_movie_details,
+                    new MovieDetailsFrag()).commit();
+        }
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public static class MovieDetailsFrag extends Fragment {
+
+        public MovieDetailsFrag() {}
+
+        public View onCreate(LayoutInflater inflate, ViewGroup container, Bundle savedInstanceState) {
+            View rootV = getLayoutInflater().inflate(R.layout.movie_details_fragment, container, false);
+            initMovieDetails(rootV);
+            setValues(rootV);
+            return rootV;
+        }
+
+        public void initMovieDetails(View rootV) {
+            movie = new Movie();
+
+            MovieDetailsActivity.intent = getActivity().getIntent();
+
+            int movie_position = intent.getIntExtra("movie_position", 0);
+            movie = MainActivity.movieList.get(movie_position);
+
+            title = (TextView) rootV.findViewById(R.id.title);
+            release = (TextView) rootV.findViewById(R.id.release_date);
+            rating = (TextView) rootV.findViewById(R.id.rating);
+            synopsis = (TextView) rootV.findViewById(R.id.synopsis);
+            poster = (ImageView) rootV.findViewById(R.id.poster);
+        }
+
+        public void setValues(View rootV) {
+            title.setText(movie.getmTitle());
+            //title.setVisibility(View.VISIBILE);
+            release.setText(movie.getmReleaseDate());
+            rating.setText(movie.getmRating());
+            synopsis.setText(movie.getmDescription());
+
+            String movie_poster_url;
+
+            if(movie.getmPoster() == MovieAPI.IMAGE_NOT_FOUND) {
+                movie_poster_url = MovieAPI.IMAGE_NOT_FOUND;
+            } else {
+                movie_poster_url = MovieAPI.IMAGE_URL + MovieAPI.IMAGE_SIZE + movie.getmPoster();
+            }
+
+            Picasso.with(rootV.getContext()).load(movie_poster_url).into(poster);
+            poster.setVisibility(View.VISIBLE);
+        }
+    }
 }
--- app/src/main/java/com/example/android/popularmovie/SettingsActivity.java
+++ app/src/main/java/com/example/android/popularmovie/SettingsActivity.java
@@ -1,4 +1,44 @@
 package com.example.android.popularmovie;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+
+public class SettingsActivity extends PreferenceActivity implements Preference.OnPreferenceChangeListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.general_preference);
+        bindPreferenceSummaryToValue(findPreference("sort_by"));
+    }
+
+    private void bindPreferenceSummaryToValue(Preference pref) {
+        pref.setOnPreferenceChangeListener(this);
+
+        onPreferenceChange(pref, PreferenceManager
+                .getDefaultSharedPreferences(pref.getContext())
+                .getString(pref.getKey(), "");
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+
+        String value = newValue.toString();
+
+        if(preference instanceof ListPreference) {
+            ListPreference listPref = (ListPreference) preference;
+
+            int prefIndex = listPref.findIndexOfValue(value);
+            if(prefIndex >= 0) {
+                preference.setSummary(listPref.getEntries()[prefIndex]);
+            }
+        } else {
+            preference.setSummary(value);
+        }
+        return true;
+    }
+
 }
